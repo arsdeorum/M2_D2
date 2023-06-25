1216de4a6ec89f2e87cca90b43f0897ab6ddfe3e
arsdeorum <arsdeorum@gmail.com>
2023-06-25 08:59:43 +0200

    M2_D2

---
+++ s1p2-scaricabile.js
@@ -0,0 +1,81 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* 
+Stringa: Una stringa è un insieme di caratteri che possono essere lettere, numeri o simboli. È come un pezzo di testo che possiamo usare per scrivere parole o frasi. Ad esempio, "hello world!" è una stringa.
+
+Numero: Un numero è un valore matematico. Possiamo usarlo per contare le cose o per fare calcoli come l'addizione o la sottrazione.
+
+Booleano: Un booleano può avere solo due valori: vero o falso. Possiamo pensarci come a una risposta a una domanda, solo che la risposta potrebbe solo essere vero o falso.
+
+Array: Un array è un insieme di valori separati da virgole e racchiusi tra parentesi quadre []. Possiamo pensarlo come a una scatola che contiene diverse cose. Ad esempio, un array potrebbe essere [1, 2, 3, 4, 5], che contiene una serie di numeri. 
+
+Undefined: Se qualcosa è "undefined" significa che non ha un valore assegnato, si può pensarlo come a un'informazione mancante o a una casella vuota. Ad esempio, se mi chiedono quale sia il mio colore preferito e non rispondo, il valore sarà "undefined" perché non ho fornito una risposta.
+
+Null: Null è simile a "undefined" nel senso che rappresenta l'assenza di valore, è un'indicazione che qualcosa non esiste o è vuoto.
+
+Data: La data rappresenta un punto nel tempo, come il giorno corrente. Possiamo usarla per tenere traccia delle informazioni temporali.
+
+*/
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* Oggetto: è una struttura di dati, che ci permette di organizzare e usare i dati stessi in modo più complesso rispetto ai semplici tipi di dato elencati prima, per concludere un'oggetto può contenere ed organizzare vari tipi di dati */
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* 
+const numero1 = 12;
+const numero2 = 20;
+const somma = numero1 + numero2;
+console.log(somma);
+*/
+
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* let x = 12 */
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* const name = "Diego Mendez" */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* 
+let x = 12
+const differenza = x - 4
+console.log(differenza); */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* const name1 = "john";
+const name2 = "John";
+
+console.log(name1 !== name2);
+console.log(name1.toLowerCase() === name2.toLowerCase()); */
